67f192b9b1e118afd13d6103548d11b252500f56
KokaAshish <ashishkoka34@gmail.com>
2024-06-19 22:57:14 +0530

    feat: set default theme

--- js/functions.js
+++ js/functions.js
@@ -7,3 +7,33 @@ function toggleTheme() {
   $("#normal-text-div").toggleClass("text-light text-dark");
   $(".span-text").toggleClass("text-info text-primary");
 }
+
+function setDarkTheme() {
+  $("body").addClass("bg-dark").removeClass("bg-light");
+  $(".theme-btn").addClass("btn-light").removeClass("btn-dark");
+  $("#normal-text-div").addClass("text-light").removeClass("text-dark");
+  $(".span-text").addClass("text-primary").removeClass("text-info");
+  $(".theme-btn").html("💡 &nbsp; Light");
+}
+
+function setLightTheme() {
+  $("body").addClass("bg-light").removeClass("bg-dark");
+  $(".theme-btn").addClass("btn-dark").removeClass("btn-light");
+  $("#normal-text-div").addClass("text-dark").removeClass("text-light");
+  $(".span-text").addClass("text-info").removeClass("text-primary");
+  $(".theme-btn").html("❍ &nbsp; Dark");
+}
+
+function setDefaultTheme() {
+  const darkTheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+  if (darkTheme.matches) {
+    setDarkTheme();
+  } else {
+    setLightTheme();
+  }
+}
+
+$(function () {
+  setDefaultTheme();
+});
